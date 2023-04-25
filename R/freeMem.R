b2cf2200eaf1047728da35ee19b07d12ed2910f1
ichangbr <ichang@alumni.unav.es>
2023-04-25 07:51:51 +0000

    more options freeMem

--- R/freeMem.R
+++ R/freeMem.R
@@ -14,11 +14,15 @@
 #' freeMem(c("a", "b"), echo = T)
 #' @export
 
-freeMem <- function(objs, echo = F){
-  if(!is.vector(objs)) stop("objs must be a vector")
-  if(!is.character(objs)) stop("objs must be a character vector")
-  if(!length(objs) > 0) stop("objs must be length 1 or higher")
-  if(!is.logical(echo) | !length(echo) == 1) stop("objs must be logical value")
+freeMem <- function(objs = NULL, echo = F){
+  if(is.null(objs)){
+    objs <- ls(envir = .GlobalEnv)
+  } else {
+    if(!is.vector(objs)) stop("objs must be a vector")
+    if(!is.character(objs)) stop("objs must be a character vector")
+    if(!length(objs) > 0) stop("objs must be length 1 or higher")
+    if(!is.logical(echo) | !length(echo) == 1) stop("objs must be logical value")
+  }
   rm(list = objs, envir = .GlobalEnv)
   if(!echo) invisible(gc()) else gc()
 }
