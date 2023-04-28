dd4007a0aee277ca637a75ecf917f25c51ff33cd
ichangbr <ichang@alumni.unav.es>
2023-04-28 13:09:59 +0000

    better freeMem

--- R/freeMem.R
+++ R/freeMem.R
@@ -25,7 +25,7 @@ freeMem <- function(objs = NULL, echo = F){
     if(!length(objs) > 0) stop("objs must be length 1 or higher")
     if(!is.logical(echo) | !length(echo) == 1) stop("objs must be logical value")
   }
-  rm(list = objs, envir = .GlobalEnv)
+  rm(list = intersect(objs, ls(envir = .GlobalEnv)), envir = .GlobalEnv)
   if(!echo) invisible(gc()) else gc()
 }
 
