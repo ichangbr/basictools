d5117208ba7945204fef43ba3fd7587a2efe5b3c
ichangbr <ichang@alumni.unav.es>
2023-05-03 08:21:43 +0000

    freeMem option to exclude

--- R/freeMem.R
+++ R/freeMem.R
@@ -2,13 +2,14 @@
 # | |___ |==]
 # ---
 # CREATED: 24.4.2023
-# LAST MODIFIED: 24.4.2023
+# LAST MODIFIED: 3.5.2023
 
 
 #' Free Memory
 #' 
 #' Deletes selected files from the environment
 #' @param objs Character vector of the names of objects to delete. If left empty all objects will be deleted
+#' @param exclude Boolean to determine whether to keep or discard the selected objects
 #' @param echo If true, output for gc() is shown. F by default
 #' @examples
 #' a <- c(1, 2)
@@ -16,15 +17,17 @@
 #' freeMem(c("a", "b"), echo = T)
 #' @export
 
-freeMem <- function(objs = NULL, echo = F){
+freeMem <- function(objs = NULL, exclude = F, echo = F){
   if(is.null(objs)){
     objs <- ls(envir = .GlobalEnv)
-  } else {
-    if(!is.vector(objs)) stop("objs must be a vector")
-    if(!is.character(objs)) stop("objs must be a character vector")
-    if(!length(objs) > 0) stop("objs must be length 1 or higher")
-    if(!is.logical(echo) | !length(echo) == 1) stop("objs must be logical value")
+  } else if (exclude) {
+    objs <- setdiff(ls(envir = .GlobalEnv), objs)
   }
+  if(!is.vector(objs)) stop("objs must be a vector")
+  if(!is.character(objs)) stop("objs must be a character vector")
+  if(!length(objs) > 0) stop("objs must be length 1 or higher")
+  if(!is.logical(echo) | !length(echo) == 1) stop("objs must be logical value")
+  
   rm(list = intersect(objs, ls(envir = .GlobalEnv)), envir = .GlobalEnv)
   if(!echo) invisible(gc()) else gc()
 }
